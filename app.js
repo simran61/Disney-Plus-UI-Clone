63830e5e15d880730517a708ab46ef8cb99d3ac7
Simran Gangwani <simrangngwani3107@gmail.com>
2021-10-03 17:27:36 +0530

    completed ui

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const carousel = document.querySelector('.carousel');
 let sliders = []
 
 let slideIndex= 0;
-var createSlide = () => {
+const createSlide = () => {
     if(slideIndex >= movies.length){
         slideIndex=0;
-    }
+    }  
 
     //creating DOM elements
     let slide = document.createElement('div');
@@ -37,8 +37,9 @@ var createSlide = () => {
     sliders.push(slide)
 
     //adding sliding effects
-    if(sliders.lenght){
-        sliders[0].style.marginLeft= `calc(-${100 * (sliders.length - 2)} - ${30 * (sliders.length - 2)}px)`;
+    
+    if(sliders.length){
+        sliders[0].style.marginLeft = `calc(-${100 * (sliders.length - 2)}% - ${30 * (sliders.length - 2)}px)`;
     }
 }
 
@@ -47,5 +48,38 @@ for(let i=0; i<3; i++){
 }
 
 setInterval(() => {
-    createSlide();
-}, 3000); 
+    // createSlide();
+}, 3000)
+
+//video cards
+const videoCards = [...document.querySelectorAll('.video-card')];
+
+videoCards.forEach( item => {
+    item.addEventListener('mouseover', () =>{
+        let video = item.children[1];
+        video.play();
+    })
+    item.addEventListener('moudseleave', () =>{
+        let video = item.children[1];
+        video.pause();
+    })
+})
+
+// card sliders
+
+let cardContainers = [...document.querySelectorAll('.card-container')];
+let preBtns = [...document.querySelectorAll('.pre-btn')];
+let nxtBtns = [...document.querySelectorAll('.nxt-btn')];
+
+cardContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtns[i].addEventListener('click', ()=>{
+        item.scrollLeft += containerWidth - 200;
+    })
+
+    preBtns[i].addEventListener('click', ()=>{
+        item.scrollLeft -= containerWidth + 200;
+    })
+})
